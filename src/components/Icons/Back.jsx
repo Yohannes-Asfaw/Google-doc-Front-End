4847a93cbb3307b3a0f45f93307d1f1d77af7eb1
Yohannes-Asfaw <jonathanasfaw11@gmail.com>
2023-09-06 16:04:21 +0300

    adding the  component for header

---
+++ src/components/Icons/Back.jsx
@@ -0,0 +1,16 @@
+export function Back(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="2em"
+      height="2em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m7.825 13l4.9 4.9q.3.3.288.7t-.313.7q-.3.275-.7.288t-.7-.288l-6.6-6.6q-.15-.15-.213-.325T4.426 12q0-.2.063-.375T4.7 11.3l6.6-6.6q.275-.275.688-.275t.712.275q.3.3.3.713t-.3.712L7.825 11H19q.425 0 .713.288T20 12q0 .425-.288.713T19 13z"
+      ></path>
+    </svg>
+  );
+}
