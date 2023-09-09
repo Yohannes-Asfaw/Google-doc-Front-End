64847a2677e48e0a050a9bba508f66cf4dae03a6
Yohannes-Asfaw <jonathanasfaw11@gmail.com>
2023-09-09 16:08:47 +0300

    adding icons for usage

---
+++ src/components/Icons/EditDoc.jsx
@@ -0,0 +1,23 @@
+export default function EditDoc(
+  props
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1.5em"
+      height="1.5em"
+      viewBox="0 0 14 14"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="#47acd4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="m7.5 9l-3 .54L5 6.5L10.73.79a1 1 0 0 1 1.42 0l1.06 1.06a1 1 0 0 1 0 1.42Z"></path>
+        <path d="M12 9.5v3a1 1 0 0 1-1 1H1.5a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3"></path>
+      </g>
+    </svg>
+  );
+}
