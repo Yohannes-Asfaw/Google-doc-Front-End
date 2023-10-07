5a173664a954a11234fbb0ba651e8bed18a649be
Yohannes-Asfaw <jonathanasfaw11@gmail.com>
2023-10-07 16:05:50 +0300

    adding icons for usage

---
+++ src/components/Icons/CreateDoc.jsx
@@ -0,0 +1,18 @@
+export default function CreateDoc(props) {
+  return (
+    <div style={{
+      textDecoration: 'none',
+      color: '#ffffff',
+      backgroundColor:  '#47acd4',
+      padding: '0px 0px',
+      fontSize: '16px',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      outline: 'none',
+    }}>
+<i className="bx bx-plus bx-sm"></i>
+    </div>
+    
+  );
+}
